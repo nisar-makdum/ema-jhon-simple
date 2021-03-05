157b57b7b83d2cffa109ecae1f32300e662706c8
nisar-makdum <abrarabir97@gmail.com>
2021-03-05 22:29:39 +0600

    some changes

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,6 +3,7 @@ import fakeData from '../../fakeData';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
+import {addToDatabaseCart} from '../../utilities/databaseManager';
 
 const Shop = () => {
 const first10 = fakeData.slice(0,10);
@@ -12,6 +13,9 @@ const[cart, setCart] = useState ([]);
 const handleAddProduct = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
+    const sameProduct = newCart.filter(pd => pd.key=== product.key);
+    const count = sameProduct.length;
+    addToDatabaseCart(product.key, count);
 }
 
     return (
@@ -19,6 +23,8 @@ const handleAddProduct = (product) => {
             <div className="product-container">
                 {
                 products.map(pd => <Product 
+                    key={pd.key}
+                    showAddToCart={true}
                     handleAddProduct = {handleAddProduct}
                     product={pd}
                     ></Product>)
